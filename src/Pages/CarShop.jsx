98c38eae1a39a47e8c7209f34f7159c7ee96dfc2
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-10-18 01:05:53 +0600

    fix: loader

--- src/Pages/CarShop.jsx
+++ src/Pages/CarShop.jsx
@@ -4,49 +4,70 @@ import { useParams } from "react-router-dom";
 const CarShop = () => {
   const params = useParams();
   const { brand_name } = params;
-  console.log(brand_name);
   const [cars, setCars] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch(`http://localhost:4000/cars/${brand_name}`)
       .then((res) => res.json())
-      .then((data) => setCars(data));
+      .then((data) => {
+        setCars(data);
+        setLoading(false);
+      });
   }, [brand_name]);
+  console.log(cars);
+
   return (
     <div className="max-w-7xl mx-auto my-10">
-      <h3 className="text-4xl lg:text-6xl  font-semibold text-center py-10 ">
-        {" "}
-        {brand_name}
-      </h3>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
-        {cars?.map((car) => (
-          <div
-            key={car._id}
-            className="card w-96 bg-base-100 shadow-xl mb-6 mx-auto md:shadow-none lg:shadow-lg"
-          >
-            <figure className="px-10 pt-10">
-              <img
-                src={car?.ImageURL}
-                alt="cars"
-                className="rounded-xl h-[170px] w-[302px]"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">
-                {car?.brand_name} {car?.Name}{" "}
-              </h2>
-              <h3>Type: {car?.type}</h3>
-              <h3>Price: {car?.price} $</h3>
-              <h3>Rating: {car?.rating} $</h3>
+      {loading ? (
+        <div className="h-screen  flex items-center">
+          {" "}
+          <span className="mx-auto loading loading-bars loading-lg"></span>
+        </div>
+      ) : cars.length > 0 ? (
+        <>
+          <h3 className="text-4xl lg:text-6xl  font-semibold text-center py-10 ">
+            {" "}
+            {brand_name}
+          </h3>
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+            {cars?.map((car) => (
+              <div
+                key={car._id}
+                className="card w-96 bg-base-100 shadow-xl mb-6 mx-auto md:shadow-none lg:shadow-lg"
+              >
+                <figure className="px-10 pt-10">
+                  <img
+                    src={car?.ImageURL}
+                    alt="cars"
+                    className="rounded-xl h-[170px] w-[302px]"
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">
+                    {car?.brand_name} {car?.Name}{" "}
+                  </h2>
+                  <h3>Type: {car?.type}</h3>
+                  <h3>Price: {car?.price} $</h3>
+                  <h3>Rating: {car?.rating} $</h3>
 
-              <div className="card-actions">
-                <button className="btn text-white bg-primary-default hover:bg-white hover:text-primary-default border-primary-default">
-                  Details
-                </button>
+                  <div className="card-actions">
+                    <button className="btn text-white bg-primary-default hover:bg-white hover:text-primary-default border-primary-default">
+                      Details
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      ) : (
+        <div className="h-screen flex items-center text-4xl lg:text-6xl text-center font-bold">
+          {" "}
+          <h3 className="lg:w-1/2 mx-auto">
+            Sorry No {brand_name} Car Availble at the moment
+          </h3>
+        </div>
+      )}
     </div>
   );
 };
