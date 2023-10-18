836a1178cb3f2da8917aef9192e10f86c7640750
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-10-19 01:29:01 +0600

    fix: footer

--- src/COmponents/Footer.jsx
+++ src/COmponents/Footer.jsx
@@ -3,21 +3,21 @@ const Footer = () => {
     <div className="bg-base-200">
       <div className="max-w-7xl mx-auto">
         <footer className="footer p-10  text-base-content">
-          <nav className="mx-auto">
+          <nav className="mx-auto flex flex-col items-center">
             <header className="footer-title">Services</header>
             <a className="link link-hover">Branding</a>
             <a className="link link-hover">Design</a>
             <a className="link link-hover">Marketing</a>
             <a className="link link-hover">Advertisement</a>
           </nav>
-          <nav className="mx-auto">
+          <nav className="mx-auto flex flex-col items-center">
             <header className="footer-title">Company</header>
             <a className="link link-hover">About us</a>
             <a className="link link-hover">Contact</a>
             <a className="link link-hover">Jobs</a>
             <a className="link link-hover">Press kit</a>
           </nav>
-          <nav className="mx-auto">
+          <nav className="mx-auto flex flex-col items-center">
             <header className="footer-title">Legal</header>
             <a className="link link-hover">Terms of use</a>
             <a className="link link-hover">Privacy policy</a>
