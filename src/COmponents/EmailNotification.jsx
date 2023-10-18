7496ca226f859ecb018de0bd3d218c6eb155c6c3
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-10-18 19:07:19 +0600

    fix: newsletter

--- src/COmponents/EmailNotification.jsx
+++ src/COmponents/EmailNotification.jsx
@@ -1,11 +1,14 @@
 const EmailNotification = () => {
   return (
     <div className="bg-gray-400 py-20 text-black">
-      <h3 className=" text-center pt-10 pb-20 md:pb-10 text-4xl md:text-5xl font-semibold">
+      <h3 className=" text-center pt-10   text-4xl md:text-5xl font-semibold">
         Subscribe to our Newsletter
       </h3>
+      <h3 className=" text-center text-primary pb-20 md:pb-10 text-2xl md:text-2xl font-semibold">
+        to get latest updates{" "}
+      </h3>
       <form>
-        <fieldset className="form-control lg:w-1/2 mx-auto">
+        <fieldset className="form-control lg:w-1/2 w-3/4 mx-auto">
           <div className="relative ">
             <input
               type="text"
